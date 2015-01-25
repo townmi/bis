15ba936ed401c22f4d816aca31437dada216fea7
townmi <towne766@126.com>
2015-01-25 22:10:39 +0800

    add at home

--- js/app.js
+++ js/app.js
@@ -26,6 +26,21 @@ define(function (require, exports, module){
 
 			// $("body").jScrollPane();
 
+		});
+
+		$(window).on('scroll', function(){
+
+			///console.log($(window).scrollTop());
+
+			if($(window).scrollTop()>34){
+				// $(".js_side").stop().animate({top: $(window).scrollTop()-34},100)
+				$(".js_side").css({"position" : "fixed"})
+			}else{
+				$(".js_side").css({"position" : "absolute"})
+			}
+
+			//$(".js_side").stop().animate({top: },100)
+
 		})
 
 	}
