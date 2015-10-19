55c74bc26aa3349506bd30615fc3389d38c7fcb4
townmi <towne766@126.com>
2015-10-19 14:57:24 +0800

    completed with search_group

--- js/custom.js
+++ js/custom.js
@@ -2,23 +2,27 @@
  // Calling the function
 $(function() {
     $('#js_toggle_nav').click(function() {
-        $("body").toggleClass("background_blur");
+        // $("body").toggleClass("background_blur");
         var bars = $("#bars");
 
         if(bars.attr("action") == "search"){
-            $(".search_group").hide();
-            $("#js_search").show();
-            $("#marsk").removeClass("active").hide();
+            $("#js_search_key_word").removeClass("active")
+            $("#marsk").removeClass("active");
+            setTimeout(function () {
+                $("#marsk").hide();
+            }, 500);
             return bars.removeClass().addClass("from_close").attr("action", "");
         }
         if(bars.hasClass("to_arrow")){
             bars.removeClass().addClass("from_arrow");
         }else{
             bars.removeClass().addClass("to_arrow");
+
         }
         toggleNavigation();
 
     });
+
     $(".content").on("scroll", function(){
         
         if($("#bars").hasClass("to_arrow")){
@@ -36,9 +40,11 @@ function toggleNavigation() {
     if ($('#container').hasClass('display_nav')) {
         // Close Nav
         $('#container').removeClass('display_nav');
+        $("#js_search").show();
     } else {
         // Open Nav
         $('#container').addClass('display_nav');
+        $("#js_search").hide();
     }
 }
 
