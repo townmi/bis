ebeb9f3b2771f57f8b54a6988c81e03912148ff4
townmi <towne766@126.com>
2017-01-23 22:43:36 +0800

    add stock view logic

--- src/routes/Stock/components/Stock.js
+++ src/routes/Stock/components/Stock.js
@@ -1,28 +1,80 @@
 import React, {Component} from 'react';
 import './stock.scss';
 
+let data = [];
 
 class Stock extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            status: "loading"
+        };
+
+        let self = this;
+
+        let promise = $.getScript("//cdn.bootcss.com/highstock/5.0.7/highstock.js");
+
+        promise = promise.then(function () {
+            return $.get("http://127.0.0.1:9090/stock")
+        });
+        promise.then(function (res) {
+            $.parseJSON(res).forEach(function (cell, index) {
+                data.push([new Date(cell.Date).getTime(), cell.CloseAtCash])
+            });
+            self.setState((prevState) => {
+                prevState.status = "complete";
+            });
+        });
+
+    }
+    componentDidUpdate() {
+        console.log(1);
+        if($ && $.fn.highcharts) {
+            $('#stockContainer').highcharts('StockChart', {
+                rangeSelector: {
+                    selected: 1
+                },
+
+                title: {
+                    text: 'AAPL Stock Price'
+                },
+
+                series: [{
+                    name: 'AAPL',
+                    data: data,
+                    tooltip: {
+                        valueDecimals: 2
+                    }
+                }]
+            });
+        }
+
+
     }
 
-    render () {
+    draw() {
+        if (this.state.status == "loading") {
+            return <div>loading</div>;
+        } else {
+            return <div id="stockContainer"></div>;
+        }
 
-        // this.setProps((prevProps) => {
-        //     prevProps.station = "home";
-        // });
+    }
+
+    render() {
+
+        console.log(this.state.status);
 
         return (
             <div className="t-home">
-                12313123
+                {this.draw()}
             </div>
         );
     }
 }
 
 Stock.propTypes = {
-    location: React.PropTypes.object.isRequired
 };
 
 export default Stock
